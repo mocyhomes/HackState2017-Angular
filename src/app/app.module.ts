c7b6afbef0d590f1f0e275a40cb7b92db4882bbc
ahtluong <aluong@crimson.ua.edu>
2017-09-23 18:08:50 -0500

    Change App

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Others
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// Component Imports
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0ss9K-oiz2p_CyN0UfQtAea7srl9PB8I",
+  authDomain: "hackstate17.firebaseapp.com",
+  databaseURL: "https://hackstate17.firebaseio.com",
+  projectId: "hackstate17",
+  storageBucket: "",
+  messagingSenderId: "329837526715"
+};
+
+const appRoutes: Routes = [
+  { path: ''        , component: DashboardComponent },
+  { path: 'login'   , component: LoginComponent     },
+  { path: 'register', component: RegisterComponent  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FlashMessagesModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
